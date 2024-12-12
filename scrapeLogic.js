c8bbfd61bc91d2d3dba328ef7a99f982a68939b6
tsmy5689 <tsmy5689@gmail.com>
2024-12-12 13:41:51 +0800

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -54,7 +54,9 @@ const scrapeLogic = async (res, url, cookieValue, proxy) => {
     page.on('request', request => {
       if (['image', 'media'].includes(request.resourceType())) {
         request.abort();
-      } else if (request.url().includes('preview.mp3')){
+      } else if (request.url().includes('sign-out?background=true')){
+         request.continue();
+      }else if (request.url().includes('preview.mp3')){
          request.continue();
       } else if (request.url().includes('analytics.google.com')){
          request.continue();
