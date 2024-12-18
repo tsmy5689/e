291b300b87ecda1f2f5052e3439b618a898909ce
tsmy5689 <tsmy5689@gmail.com>
2024-12-18 14:48:24 +0800

    Update login.js

--- login.js
+++ login.js
@@ -37,6 +37,7 @@ const initializeBrowser = async (proxy) => {
 
 const go = async (res, url, user,password, proxy) => {
    console.log('Page 222');
+  console.log(url);
   try {
     const browser = await initializeBrowser(proxy);
     const page = await browser.newPage();
