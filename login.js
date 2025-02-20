dd82cd5eda1b7253bb00a0d381b43acba741786a
tsmy5689 <tsmy5689@gmail.com>
2025-02-20 12:40:47 +0800

    Update login.js

--- login.js
+++ login.js
@@ -79,7 +79,7 @@ const go = async (res, url, user,pass, proxy) => {
     await page.waitForSelector('#username');
     // Type into the input field
        console.log('usename found');
-    await page.type('#username', 'tsmy5689@gmail.com');
+    await page.type('#username', 'myer5623');
     console.log('username type!');
 
      await page.waitForSelector('#password');
