68898a65082bb96916d4ce9c4c5728a09ac9097e
tsmy5689 <tsmy5689@gmail.com>
2025-05-15 23:28:36 +0800

    Update scrapeLogic2.js

--- scrapeLogic2.js
+++ scrapeLogic2.js
@@ -27,7 +27,7 @@ const initializeBrowser = async (proxy) => {
           ? process.env.PUPPETEER_EXECUTABLE_PATH
           : puppeteer.executablePath(),
       ignoreHTTPSErrors: true,
-      userDataDir: '/mnt/data/envato2222222two'
+      userDataDir: '/mnt/data/envato2222222two2'
     });
     console.log('Browser initialized');
   }
