0f303921bd0b3ff323cf1cb99a656a0cc9ab5b0d
tsmy5689 <tsmy5689@gmail.com>
2025-05-16 13:19:13 +0800

    Update login3.js

--- login3.js
+++ login3.js
@@ -7,7 +7,7 @@ const proxyPassword = '626he4yucyln';
 
 // Browserless.io connection details
 const browserlessToken = '2SJiFPoA1LWJf1efdb4c24535106e69a00f371f46e6ed5015'; // Use from .env file or fallback
-const browserlessEndpoint = `wss://production-sfo.browserless.io?token=${browserlessToken}`&proxy=residential;
+const browserlessEndpoint = `wss://production-sfo.browserless.io?token=${browserlessToken}&proxy=residential`;
 
 // Singleton browser instance
 let browser;
